699b580526a3705d502c213eaa0fa0e573b67bd1
Jacari Boboye <jboboye7@gmail.com>
2019-07-20 14:01:31 -0400

    Initial Commit

---
+++ NikeAssessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NikeAssessment
+//
+//  Created by Jacari Boboye on 7/20/19.
+//  Copyright © 2019 Jacari Boboye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
